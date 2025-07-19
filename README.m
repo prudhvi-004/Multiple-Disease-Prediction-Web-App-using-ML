cadae561e3da08c7b2560838795d2ab3bd21f2d7
Reddy Harsha Sai Venkata Prudhvi <prudhvi.bablu04@gmail.com>
2025-07-19 10:24:22 +0530

    README.m

--- README.m
+++ README.m
@@ -1,4 +1,4 @@
-🩺 Multiple Disease Prediction Web App
+#🩺 Multiple Disease Prediction Web App Using Machine Learning..
 
 
 This is a Streamlit-based web application that predicts the likelihood of multiple diseases based on user inputs. The app uses trained machine learning models to provide early warning signals for:
@@ -11,4 +11,4 @@ This is a Streamlit-based web application that predicts the likelihood of multip
 
 🚀 Live Demo
 🌐 Click here to open the app
----------------(https://multiple-disease-prediction-webapp-using-ml.streamlit.app/)--------------------
+---------------(https://multiple-disease-prediction-webapp-using-ml.streamlit.app/) --------------------
